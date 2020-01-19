187a75ac6151562c7384308155e5f9e3d7a88d11
scarvalho1 <scarvalho1@lenovo.com>
2020-01-19 14:36:21 -0800

    Add picture attachment to the Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@
 class Product < ApplicationRecord
   validates :price, :name, :description, :quantity_in_stock, presence: true
   validates :name, uniqueness: { message: 'name is already taken' }
+  has_one_attached :picture
 end
