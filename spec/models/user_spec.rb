50747ad00b1cce1415319140620c8c8dcbe59d2b
Sanmoo <sanmoo.cs@gmail.com>
2020-01-19 04:41:18 -0800

    Test user model and home controller
    Also sets up factory_bot gem and devise to work with rails and rspec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with password and username' do
+    expect(build(:user)).to be_valid
+  end
+
+  it 'is not valid without a password' do
+    expect(build(:user, password: '')).not_to be_valid
+  end
+
+  it 'is not valid without email' do
+    expect(build(:user, email: '')).not_to be_valid
+  end
+
+  it 'is not valid with an email smaller than 6 characters' do
+    expect(build(:user, password: '12345')).not_to be_valid
+  end
 end
